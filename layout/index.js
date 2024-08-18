38922ab8b68db0d2baee7110ba33f3e4cc353217
naygomes <nayara.gomes13@poli.ufrj.br>
2024-08-18 18:52:44 -0300

    change name function

--- layout/index.js
+++ layout/index.js
@@ -1,8 +1,10 @@
-function changeName() {
-  const element = document.getElementById("title");
-  element.innerHTML = "New title!";
+function changeDescription() {
+  const element = document.getElementById("description");
+  element.innerHTML =
+    "Projeto de Graduação apresentado ao Curso de Engenharia de Computação e Informação da Escola Politécnica, Universidade Federal do Rio de Janeiro, como parte dos requisitos necessários à obtenção do título de Engenheira. \n O projeto foi finalizado e apresentado no ano de 2024.";
 }
 
 window.onload = function () {
-  document.getElementById("button").focus();
+  document.getElementById("about").focus();
+  console.log(document.getElementById("about"));
 };
